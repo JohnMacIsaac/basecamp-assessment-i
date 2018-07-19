181510dbb867a61f54cc3e0a58d73c64cfd62a1e
John MacIsaac <johnmi@protonmail.com>
2018-07-18 17:55:45 -0700

    prev code passed VSCode problems but not the jasmine tests. Corrected errors in assessment.js

--- assessment.js
+++ assessment.js
@@ -12,15 +12,17 @@ let lovesCode = true;
 
 // #4 Using the previously created variables, create a new variable called 'me' that uses the myName and faveNum varible to create a sentence like this 'My name is Bryan and my favorite number is 3'
 
-let me = `My name is ${myName} and my favorite number is ${faveNum}.`
+// let me = `My name is ${myName} and my favorite number is ${faveNum}.` // rejected by test 
+let me = "My name is " + myName + " and my favorite number is " + faveNum
 
 // #5 Using the previously created faveNum variable, create a new variable called 'trueFaveNum'. *trueFaveNum* should equal true when comparing faveNum to your favorite number.v // HINT: Use a comparison operator
 
-let truFaveNum = faveNum === 3.14159;
+let trueFaveNum = faveNum === 3.14159;
 
 // #6 Create a new variable called notHakeem. Using the previously created myName variable, test that your name DOES NOT equal the string 'Hakeem Olajuwon'. If your name does happen to be Hakeem Olajuwon, test against 'Karl Malone'
 
 let notHakeem = myName !== 'Hakeem Olajuwon';
+
 
 // #7 Create an if statement that checks the age variable below. If age is greater than 16, then set canDrive to true otherwise set canDrive to false. The canDrive variable will be set by the unit test so do not adjust it.
 var age = 18;
@@ -53,5 +55,6 @@ let sum = function sum (param) {
 // #10 Create a function called 'greeter' that takes in two parameters, name and number. Have 'greeter' return a string that says something like this 'Hi! I am Bryan and I am 28 years old" but with the appropriate values..
 
 let greeter = function greeter (name, number) {
-  return `Hi! I am ${myName} and I am 35 years old`
+  //return `Hi! I am ${myName} and I am 35 years old` // test rejected
+  return "Hi! I am " + name + " and I am " + number + " years old"
 }
